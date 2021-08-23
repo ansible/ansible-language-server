08c077ae6f0ab40719a54bb7a0b00014d3d9dda7
Priyam Sahoo <priyamsahoo.26@gmail.com>
2021-08-23 13:57:20 +0530

    Add utility function for library checking

---
+++ src/utils/libraryChecker.ts
@@ -0,0 +1,29 @@
+import * as child_process from 'child_process';
+import { ExecException } from 'child_process';
+import { promisify } from 'util';
+
+const exec = promisify(child_process.exec);
+
+export async function libraryChecker(
+  libraryName: string,
+  checkArg: string
+): Promise<boolean> {
+  let success = true;
+
+  try {
+    await exec(`${libraryName} ${checkArg}`, {
+      encoding: 'utf-8',
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      error as ExecException & {
+        // according to the docs, these are always available
+        stdout: string;
+        stderr: string;
+      };
+      success = false;
+    }
+  }
+
+  return success;
+}
