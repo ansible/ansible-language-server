e5e2b8a95d3cd80c81c778f356292646f6d65bb8
Priyam Sahoo <priyamsahoo.26@gmail.com>
2022-02-24 10:09:47 +0530

    add test for yaml validation

--- test/providers/validationProvider.test.ts
+++ test/providers/validationProvider.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { Position } from 'vscode';
+import { getYamlValidation } from '../../src/providers/validationProvider';
 import {
   createTestWorkspaceManager,
   getDoc,
@@ -73,6 +74,61 @@ describe('doValidate()', () => {
             expect(actDiag.severity).to.equal(expDiag.severity);
             expect(actDiag.source).to.equal(expDiag.source);
           });
+        });
+      });
+    });
+  });
+
+  describe('YAML diagnostics', () => {
+    const tests = [
+      {
+        name: 'invalid YAML',
+        file: 'diagnostics/invalid_yaml.yml',
+        diagnosticReport: [
+          {
+            severity: 1,
+            message: 'Nested mappings are not allowed',
+            range: {
+              start: { line: 6, character: 13 } as Position,
+              end: {
+                line: 6,
+                character: 13,
+              } as Position,
+            },
+            source: 'Ansible [YAML]',
+          },
+          {
+            severity: 1,
+            message: 'Document contains trailing content',
+            range: {
+              start: { line: 7, character: 0 } as Position,
+              end: {
+                line: 8,
+                character: 0,
+              } as Position,
+            },
+            source: 'Ansible [YAML]',
+          },
+        ],
+      },
+    ];
+
+    tests.forEach(({ name, file, diagnosticReport }) => {
+      it(`should provide diagnostic for ${name}`, async function () {
+        const textDoc = await getDoc(file);
+
+        const actualDiagnostics = getYamlValidation(textDoc);
+
+        expect(actualDiagnostics.length).to.equal(diagnosticReport.length);
+
+        actualDiagnostics.forEach((diag, i) => {
+          const actDiag = diag;
+          const expDiag = diagnosticReport[i];
+
+          expect(actDiag.message).include(expDiag.message);
+          expect(actDiag.range).to.deep.equal(expDiag.range);
+          expect(actDiag.severity).to.equal(expDiag.severity);
+          expect(actDiag.source).to.equal(expDiag.source);
         });
       });
     });
