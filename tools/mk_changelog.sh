433acd7319a0e0549434c84a8959818c28f05ed6
Sorin Sbarnea <ssbarnea@redhat.com>
2022-06-21 08:46:59 +0100

    Update release task

--- tools/mk_changelog.sh
+++ tools/mk_changelog.sh
@@ -16,6 +16,9 @@ sed -i '' -e '/<!-- KEEP-THIS-COMMENT -->/r out/next.md' CHANGELOG.md
 # use prettier to reformat the changelog, lik rewrapping long lines
 npx prettier -w CHANGELOG.md
 
+# update version
+npm version "${RELEASE_NAME}" --allow-same-version --no-commit-hooks --no-git-tag-version
+
 # commit the release
 git add package.json package-lock.json CHANGELOG.md
 
@@ -23,10 +26,20 @@ git add package.json package-lock.json CHANGELOG.md
 task lint
 
 # create new release branch
-git checkout -b "release/${RELEASE_NAME}"
+git checkout -B "release/${RELEASE_NAME}"
 
 # commit the changes
 echo "Release ${RELEASE_NAME}" | cat -  out/next.md | git commit --file -
+
+# on interactive shells we want to ask for user import
+if [[ $- == *i* ]]; then
+    read -p "Are you sure you want to push and create pull-request? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    fi
+fi
 
 # do push the new branch
 git push origin "release/${RELEASE_NAME}"
